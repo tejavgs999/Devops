c699e5f43f68a923ea9134a27a34ab2ed2025f35
Vallamkonda <vallra5@RavitejaVallamkonda.local>
2018-08-19 16:40:04 -0700

    all2

--- WeHo/Instances/Master/instance1.tf
+++ WeHo/Instances/Master/instance1.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   shared_credentials_file = "/home/ubuntu/.aws/credentials"
   profile = "default"
-  region = "${var.vpc-region}"
+  region = "ap-southease-1" #"${var.vpc-region}"
 }
 
 terraform {
